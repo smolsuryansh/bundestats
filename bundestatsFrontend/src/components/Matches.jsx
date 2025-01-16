552b16063b37169f4c6f11968f982557dc727b0f
Suryansh Dubey <suryanshdubey2525@gmail.com>
2025-01-16 16:19:06 +0530

    add match details

--- bundestatsFrontend/src/components/Matches.jsx
+++ bundestatsFrontend/src/components/Matches.jsx
@@ -171,8 +171,8 @@ const Matches = ({ searchTerm }) => {
                             {match.referees && match.referees.length > 0 ? match.referees[0].name : 'Not Available'}
                           </a>
                         </th>
-                        <td className='border px-2 py-2'>
-                          <a href={`https://google.com/search?q=${match.homeTeam.name} vs ${match.awayTeam.name} ${new Date(match.utcDate).toISOString().split('T')[0]}`} target='_blank' rel="noopener noreferrer" className='text-blue-500 underline text-center'>View details</a>
+                        <td className='border px-2 py-2 text-center'>
+                          <a href={`https://google.com/search?q=${match.homeTeam.name} vs ${match.awayTeam.name} ${new Date(match.utcDate).toISOString().split('T')[0]}`} target='_blank' rel="noopener noreferrer" className='text-blue-500 underline'>View details</a>
                         </td>
                       </tr>
                     ))}
